2f74f6bb8b7cb2d81331cd98cfab5eade8bb576d
Ashik <ashikur1993@gmail.com>
2023-10-20 16:43:20 +0600

    Update

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -4,10 +4,10 @@ import React from "react";
 export default function SignUp() {
     return (
         <>
-            <div className="py-[60px] lg:py-20 xl:py-[100px] 2xl:py-[120px]">
-                <div className="container px-5">
-                    <div className="flex gap-16 items-center">
-                        <div className="w-full">
+            <div className="py-[60px] lg:py-20 xl:py-[100px] 2xl:py-[120px] px-5">
+                <div className="container ">
+                    <div className="flex gap-20 items-center">
+                        <div className="w-full pe-10">
                             <h3 className="text-[26px] md:text-[32px] lg:text-[36px] xl:text-[40px] text-dark_1 leading-[1.25em] font-bold mb-3 md:mb-5">
                                 Add a profile
                             </h3>
@@ -23,7 +23,7 @@ export default function SignUp() {
                                 expand their profile.`}
                             </p>
                         </div>
-                        <div className="w-full form-wrapper">
+                        <div className="w-full form-wrapper ">
                             <div className="p-10 rounded-xl shadow-[5px_10px_30px_0px_rgba(0,0,0,0.07)]">
                                 <h4 className="text-dark_1 font-extrabold text-[26px] leading-9 text-center mb-7">
                                     Sign up
@@ -51,9 +51,9 @@ export default function SignUp() {
                                             <label className=" text-sm leading-[1.7em] text-dark_3 mt-[10px] flex gap-[10px]">
                                                 <input
                                                     type="checkbox"
-                                                    className=" w-[22px] h-[22px]"
+                                                    className="w-5 h-5 text-[#596B36] appearance-none border border-[#596B36] checked:bg-[#596B36] checked:border-0 rounded-md bg-white"
                                                 />
-                                                I don't know their email
+                                                {`I don't know their email`}
                                             </label>
                                         </div>
                                         <div className="">
@@ -64,9 +64,13 @@ export default function SignUp() {
                                             />
                                         </div>
                                         <div className="">
+                                            <p className=" text-xs text-dark_2 leading-[1.33em] uppercase font-bold mb-[10px]">
+                                                In a few words, what does this
+                                                guide offer?
+                                            </p>
                                             <textarea
                                                 className="py-5 px-6 rounded-xl h-[120px] w-full text-base font-medium leading-5 border-2 border-dark_6 text-dark_2 outline-none focus:border-dark_4 transition-all duration-150 placeholder:text-dark_4 resize-none"
-                                                placeholder="Guide's website"
+                                                placeholder="Type hare"
                                             />
                                         </div>
                                         <div className="">
