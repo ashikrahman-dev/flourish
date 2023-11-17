e724eb99183d80b368f2dbd8407116aa50ef1486
Ashik Rahman <ashikur1993@gmail.com>
2023-11-17 15:38:04 +0600

    Update

--- src/middleware.js
+++ src/middleware.js
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 // This function can be marked `async` if using `await` inside
 
 export function middleware(request) {
-    const isAuthenticated = false;
+    const isAuthenticated = true;
 
     if (!isAuthenticated) {
         const url = new URL("/login", request.url);
@@ -16,5 +16,5 @@ export function middleware(request) {
 // See "Matching Paths" below to learn more
 
 export const config = {
-    matcher: ["/about", "/add-offerings"],
+    matcher: ["/add-offerings"],
 };
